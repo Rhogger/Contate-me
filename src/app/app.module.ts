5582216f8e24fe7e32aaf0bbdc14ede6a4d8a4ad
rhogger <rhoggerrv@gmail.com>
2023-05-02 20:46:48 -0300

    :construction: Criado o primeiro componente (1° section) e adicionado as imagens principais ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { CardsComponent } from './cards/cards.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, HomeComponent, CardsComponent],
+	imports: [BrowserModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
