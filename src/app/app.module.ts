fbbeb37359879a290924e030ffd02cfddc619119
rhogger <rhoggerrv@gmail.com>
2023-05-07 23:33:49 -0300

    :rocket: Projeto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
-import { CardsComponent } from './cards/cards.component'
+import { CardsComponent } from './cards/cards.component';
+import { ContactComponent } from './contact/contact.component'
 
 @NgModule({
-	declarations: [AppComponent, HomeComponent, CardsComponent],
+	declarations: [AppComponent, HomeComponent, CardsComponent, ContactComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
